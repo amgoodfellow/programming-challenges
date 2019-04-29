1643f0a88ccdc2e34b40a86f37c2d2e87c15d973
TheAbsoluteValue <37089733+TheAbsoluteValue@users.noreply.github.com>
2019-04-29 13:19:27 -0400

    Adds Will's solution
    Adds Will's EXTRAVAGANTLY ELEGANT solution

---
+++ min_change/TheAbsoluteValue.js
@@ -0,0 +1,13 @@
+function hi4(amt, coins) {
+	const temp = amt;
+	if (!coins.length) return 0;
+	let out = 0;
+	for (let i = 0; i < coins.length; i++) {
+		if (amt === 0) break;
+		const blah = Math.floor(amt / coins[i]);
+		if (blah === 0) continue;
+		amt -= blah * coins[i];
+		out += blah;
+    }
+	return Math.min(...([out, hi4(temp, coins.slice(1))]).filter(val => val != 0))
+}
